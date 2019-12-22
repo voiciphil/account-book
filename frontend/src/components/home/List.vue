cbdaf52a95d2409dd1853b06da876b0961e3d3a9
Seong-phil Park <voiciphil@gmail.com>
2019-12-22 23:54:06 +0900

    Add date picker

---
+++ frontend/src/components/home/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-row
+      justify="center"
+      class="mt-5"
+    >
+      <v-date-picker
+        v-model="picker"
+        color="indigo darken-3"
+        landscape
+      >
+      </v-date-picker>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      picker: new Date().toISOString().substr(0, 10),
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <List/>
   </div>
 </template>
 
 <script>
+import List from '@/components/home/List.vue';
+
 export default {
   name: 'home',
   components: {
-
+    List,
   },
 };
 </script>
