679b77fe2d0159c7f86f994b40d98571f21f55aa
Seong-phil Park <voiciphil@gmail.com>
2019-12-23 12:10:07 +0900

    Modify date picker and add radio button

--- frontend/src/components/home/List.vue
+++ frontend/src/components/home/List.vue
@@ -4,12 +4,51 @@
       justify="center"
       class="mt-5"
     >
-      <v-date-picker
-        v-model="picker"
-        color="indigo darken-3"
-        landscape
-      >
-      </v-date-picker>
+      <v-col cols="2">
+        <v-menu
+          v-model="menu"
+          v-bind:close-on-content-click="false"
+          v-bind:nudge-right="40"
+          transition="scale-transition"
+          offset-y
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              class="mt-3"
+              v-model="date"
+              prepend-icon="mdi-calendar"
+              readonly
+              outlined
+              dense
+              v-on="on"
+              color="indigo darken-3"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+            v-on:input="menu = false"
+            color="indigo darken-3"
+          ></v-date-picker>
+        </v-menu>
+      </v-col>
+      <v-col cols="2">
+        <v-radio-group
+          v-model="radio"
+          row
+        >
+          <v-radio
+            label="수입"
+            color="indigo darken-3"
+            value="0"
+          ></v-radio>
+          <v-radio
+            label="지출"
+            color="indigo darken-3"
+            value="1"
+          ></v-radio>
+        </v-radio-group>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -18,7 +57,9 @@
 export default {
   data() {
     return {
-      picker: new Date().toISOString().substr(0, 10),
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
+      radio: '0',
     };
   },
 };
