e5608ef5bf4ce4659a75d6ec5895a42b4371aebf
Seong-phil Park <voiciphil@gmail.com>
2020-01-05 16:25:15 +0900

    Add category filter

--- frontend/src/components/home/Filters.vue
+++ frontend/src/components/home/Filters.vue
@@ -4,8 +4,11 @@
       justify="center"
       class="mt-5"
     >
-      <v-col cols="3">
+      <v-col
+        cols="3"
+      >
         <v-radio-group
+          class="ml-12"
           v-model="radio"
           row
         >
@@ -26,20 +29,22 @@
           />
         </v-radio-group>
       </v-col>
-      <v-col cols="2">
+      <v-col
+        cols="3"
+      >
         <v-menu
           v-model="menu"
           v-bind:close-on-content-click="false"
           v-bind:nudge-right="40"
           transition="scale-transition"
           offset-y
-          min-width="290px"
+          min-width="300px"
         >
           <template v-slot:activator="{ on }">
             <v-text-field
               class="mt-3"
               v-model="date"
-              prepend-icon="mdi-calendar"
+              prepend-inner-icon="mdi-calendar"
               readonly
               outlined
               dense
@@ -54,6 +59,18 @@
           />
         </v-menu>
       </v-col>
+      <v-col
+        cols="3"
+      >
+        <v-select
+          class="mt-3"
+          v-bind:items="categories"
+          label="카테고리"
+          color="indigo darken-3"
+          outlined
+          dense
+        />
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -65,7 +82,24 @@ export default {
       date: new Date().toISOString().substr(0, 10),
       menu: false,
       radio: '0',
+      categories: [],
     };
+  },
+  created() {
+    this.getCategories();
+  },
+  methods: {
+    async getCategories() {
+      const res = await this.$axios.post('http://localhost:3000/api/categories/all', {
+        token: localStorage.token,
+      });
+      // this.categories = [];
+      await res.data.data.forEach((item) => {
+        this.categories.push(item.category);
+        console.log(item.category);
+      });
+      console.log(this.categories);
+    },
   },
 };
 </script>
