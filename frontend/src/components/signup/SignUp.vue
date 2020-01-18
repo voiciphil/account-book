57201cfde9bff1c98fedfef7a6a74a797a414a7b
Seong-phil Park <voiciphil@gmail.com>
2020-01-18 20:40:51 +0900

    Add exception handler in the sign in component

--- frontend/src/components/signup/SignUp.vue
+++ frontend/src/components/signup/SignUp.vue
@@ -8,10 +8,14 @@
         <v-text-field
           v-model="id"
           v-on:keyup.enter="signUp"
+          v-bind:rules="[rules.required]"
+          v-bind:error="errors.id"
           color="indigo darken-3"
           label="아이디"
           outlined
           dense
+          counter
+          maxlength="20"
         />
       </v-col>
     </v-row>
@@ -23,11 +27,15 @@
         <v-text-field
           v-model="pw"
           v-on:keyup.enter="signUp"
+          v-bind:rules="[rules.required]"
+          v-bind:error="errors.pw"
           color="indigo darken-3"
           label="비밀번호"
           type="password"
           outlined
           dense
+          counter
+          maxlength="20"
         />
       </v-col>
     </v-row>
@@ -39,11 +47,15 @@
         <v-text-field
           v-model="repw"
           v-on:keyup.enter="signUp"
+          v-bind:rules="[rules.required, rules.different]"
+          v-bind:error="errors.repw"
           color="indigo darken-3"
           label="비밀번호 재입력"
           type="password"
           outlined
           dense
+          counter
+          maxlength="25"
         />
       </v-col>
     </v-row>
@@ -70,18 +82,33 @@ export default {
       id: '',
       pw: '',
       repw: '',
+      rules: {
+        required: value => !!value || '입력해 주세요.',
+        different: value => value === this.pw || '비밀번호가 일치하지 않습니다.',
+      },
+      errors: {
+        id: false,
+        pw: false,
+        repw: false,
+      },
     };
   },
   methods: {
     async signUp() {
-      if (this.pw === this.repw) {
-        const res = await this.$axios.post('http://localhost:3000/api/auth/sign-up', {
-          id: this.id,
-          pw: this.pw,
-        });
+      this.errors.id = this.id === '';
+      this.errors.pw = this.pw === '';
+      this.errors.repw = this.repw === '';
 
-        if (res.data.success) {
-          this.$router.push('/');
+      if (!this.errors.id && !this.errors.pw && !this.errors.repw) {
+        if (this.pw === this.repw) {
+          const res = await this.$axios.post('http://localhost:3000/api/auth/sign-up', {
+            id: this.id,
+            pw: this.pw,
+          });
+
+          if (res.data.success) {
+            this.$router.push('/');
+          }
         }
       }
     },
