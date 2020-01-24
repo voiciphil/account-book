0cbd13b1475eab2f3454eae19d51b80457465cee
Seong-phil Park <voiciphil@gmail.com>
2020-01-24 12:13:42 +0900

    Modify a method of exception handling in sign up page

--- frontend/src/components/signup/SignUp.vue
+++ frontend/src/components/signup/SignUp.vue
@@ -16,8 +16,6 @@
             <v-text-field
               v-model="id"
               v-on:keyup.enter="signUp"
-              v-bind:rules="[rules.duplicated, rules.required]"
-              v-bind:error="errors.id"
               color="light-green lighten-1"
               label="아이디"
               outlined
@@ -35,8 +33,6 @@
             <v-text-field
               v-model="pw"
               v-on:keyup.enter="signUp"
-              v-bind:rules="[rules.required]"
-              v-bind:error="errors.pw"
               color="light-green lighten-1"
               label="비밀번호"
               type="password"
@@ -55,8 +51,6 @@
             <v-text-field
               v-model="repw"
               v-on:keyup.enter="signUp"
-              v-bind:rules="[rules.required, rules.different]"
-              v-bind:error="errors.repw"
               color="light-green lighten-1"
               label="비밀번호 재입력"
               type="password"
@@ -96,40 +90,29 @@ export default {
       id: '',
       pw: '',
       repw: '',
-      rules: {
-        required: value => !!value || '입력해 주세요.',
-        different: value => value === this.pw || '비밀번호가 일치하지 않습니다.',
-        duplicated: () => !this.errors.duplicated || '아이디가 중복되었습니다.',
-      },
-      errors: {
-        id: false,
-        pw: false,
-        repw: false,
-        duplicated: false,
-      },
     };
   },
   methods: {
     async signUp() {
-      this.errors.id = this.id === '';
-      this.errors.pw = this.pw === '';
-      this.errors.repw = this.repw === '';
-
-      if (!this.errors.id && !this.errors.pw && !this.errors.repw) {
+      if (this.id !== '' && this.pw !== '' && this.repw !== '') {
         if (this.pw === this.repw) {
-          const res = await this.$axios.post('http://localhost:3000/api/auth/sign-up', {
-            id: this.id,
-            pw: this.pw,
-          });
-
-          if (res.data.success) {
+          try {
+            await this.$axios.post('http://localhost:3000/api/auth/sign-up', {
+              id: this.id,
+              pw: this.pw,
+            });
             this.$router.push('/signed-up');
-          } else {
-            this.errors.duplicated = true;
-            this.errors.id = true;
-            this.id = '';
+          } catch (err) {
+            // eslint-disable-next-line no-alert
+            alert('아이디가 중복되었습니다.');
           }
+        } else {
+          // eslint-disable-next-line no-alert
+          alert('비밀번호가 다릅니다.');
         }
+      } else {
+        // eslint-disable-next-line no-alert
+        alert('빈 칸 없이 입력해 주세요.');
       }
     },
   },
